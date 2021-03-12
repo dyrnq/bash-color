e86e558799100bd227bbff4d5ee25e6b87740aaf
dyrnq <dyrnq@qq.com>
2021-03-13 01:06:53 +0800

    Update bash-color.sh

--- bash-color.sh
+++ bash-color.sh
@@ -1,129 +1,129 @@
 #!/usr/bin/env bash
-jeshile='\e[40;38;5;82m'
-red='\e[31m'
-jo='\e[0m'
-yellow='\e[0;93m'
+jeshile='\033[40;38;5;82m'
+red='\033[31m'
+jo='\033[0m'
+yellow='\033[0;93m'
 
 
 echo " "
-echo -e "${jeshile} ################################################ \e[0m"
-echo -e "${jeshile} #            BASH COLOR                        # \e[0m"
-echo -e "${jeshile} ################################################ \e[0m"
+echo -e "${jeshile} ################################################ \033[0m"
+echo -e "${jeshile} #            BASH COLOR                        # \033[0m"
+echo -e "${jeshile} ################################################ \033[0m"
 echo " "
 
 echo " "
-echo -e "${red} ################################################ \e[0m"
-echo -e "${red} #            BASH COLOR                        # \e[0m"
-echo -e "${red} ################################################ \e[0m"
+echo -e "${red} ################################################ \033[0m"
+echo -e "${red} #            BASH COLOR                        # \033[0m"
+echo -e "${red} ################################################ \033[0m"
 echo " "
 
 
 echo " "
-echo -e "${jo} ################################################ \e[0m"
-echo -e "${jo} #            BASH COLOR                        # \e[0m"
-echo -e "${jo} ################################################ \e[0m"
+echo -e "${jo} ################################################ \033[0m"
+echo -e "${jo} #            BASH COLOR                        # \033[0m"
+echo -e "${jo} ################################################ \033[0m"
 echo " "
 
 echo " "
-echo -e "${yellow} ################################################ \e[0m"
-echo -e "${yellow} #            BASH COLOR                        # \e[0m"
-echo -e "${yellow} ################################################ \e[0m"
+echo -e "${yellow} ################################################ \033[0m"
+echo -e "${yellow} #            BASH COLOR                        # \033[0m"
+echo -e "${yellow} ################################################ \033[0m"
 echo " "
 
 
-NOCOLOR='\e[0m'
-RED='\e[0;31m'
-GREEN='\e[0;32m'
-ORANGE='\e[0;33m'
-BLUE='\e[0;34m'
-PURPLE='\e[0;35m'
-CYAN='\e[0;36m'
-LIGHTGRAY='\e[0;37m'
-DARKGRAY='\e[1;30m'
-LIGHTRED='\e[1;31m'
-LIGHTGREEN='\e[1;32m'
-YELLOW='\e[1;33m'
-LIGHTBLUE='\e[1;34m'
-LIGHTPURPLE='\e[1;35m'
-LIGHTCYAN='\e[1;36m'
-WHITE='\e[1;37m'
+#NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPURPLE='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
 
-echo -e "${RED}bash color ${NOCOLOR}"
-echo -e "${GREEN}bash color ${NOCOLOR}"
-echo -e "${ORANGE}bash color ${NOCOLOR}"
-echo -e "${BLUE}bash color ${NOCOLOR}"
-echo -e "${PURPLE}bash color ${NOCOLOR}"
-echo -e "${CYAN}bash color ${NOCOLOR}"
-echo -e "${LIGHTGRAY}bash color ${NOCOLOR}"
-echo -e "${DARKGRAY}bash color ${NOCOLOR}"
-echo -e "${LIGHTRED}bash color ${NOCOLOR}"
-echo -e "${LIGHTGREEN}bash color ${NOCOLOR}"
-echo -e "${YELLOW}bash color ${NOCOLOR}"
-echo -e "${LIGHTBLUE}bash color ${NOCOLOR}"
-echo -e "${LIGHTPURPLE}bash color ${NOCOLOR}"
-echo -e "${LIGHTCYAN}bash color ${NOCOLOR}"
-echo -e "${WHITE}bash color ${NOCOLOR}"
+echo -e "${RED}bash color \033[0m"
+echo -e "${GREEN}bash color \033[0m"
+echo -e "${ORANGE}bash color \033[0m"
+echo -e "${BLUE}bash color \033[0m"
+echo -e "${PURPLE}bash color \033[0m"
+echo -e "${CYAN}bash color \033[0m"
+echo -e "${LIGHTGRAY}bash color \033[0m"
+echo -e "${DARKGRAY}bash color \033[0m"
+echo -e "${LIGHTRED}bash color \033[0m"
+echo -e "${LIGHTGREEN}bash color \033[0m"
+echo -e "${YELLOW}bash color \033[0m"
+echo -e "${LIGHTBLUE}bash color \033[0m"
+echo -e "${LIGHTPURPLE}bash color \033[0m"
+echo -e "${LIGHTCYAN}bash color \033[0m"
+echo -e "${WHITE}bash color \033[0m"
 
-# sed code 
+# set unset 
 # 1 21 Bold/Bright	
 # 2 22 Dim
 # 4 24 Underlined	
 # 5 25 Blink
 # 7 27 Reverse (invert the foreground and background colors)	
 # 8 28 Hidden (useful for passwords)	
-# 0 rest all
+# 0 reset all
 
-echo -e "Normal \e[1mBold ${NOCOLOR}"
-echo -e "Normal \e[2mDim ${NOCOLOR}"
-echo -e "Normal \e[4mUnderlined ${NOCOLOR}"
-echo -e "Normal \e[5mBlink ${NOCOLOR}"
-echo -e "Normal \e[7minverted ${NOCOLOR}"
-echo -e "Normal \e[8mHidden ${NOCOLOR}"
-
-
-echo -e "Normal \e[39mDefault ${NOCOLOR}"
-echo -e "Normal \e[30mBlack ${NOCOLOR}"
-echo -e "Normal \e[31mRed ${NOCOLOR}"
-echo -e "Normal \e[32mGreen ${NOCOLOR}"
-echo -e "Normal \e[33mYellow ${NOCOLOR}"
-echo -e "Normal \e[34mBlue ${NOCOLOR}"
-echo -e "Normal \e[35mMagenta ${NOCOLOR}"
-echo -e "Normal \e[36mCyan ${NOCOLOR}"
-echo -e "Normal \e[37mLight gray ${NOCOLOR}"
-echo -e "Normal \e[90mDark gray ${NOCOLOR}"
-echo -e "Normal \e[91mLight red ${NOCOLOR}"
-echo -e "Normal \e[92mLight green ${NOCOLOR}"
-echo -e "Normal \e[93mLight yellow ${NOCOLOR}"
-echo -e "Normal \e[94mLight blue ${NOCOLOR}"
-echo -e "Normal \e[95mLight magenta ${NOCOLOR}"
-echo -e "Normal \e[96mLight cyan ${NOCOLOR}"
-echo -e "Normal \e[97mWhite ${NOCOLOR}"
+echo -e "Normal \033[1mBold \033[0m"
+echo -e "Normal \033[2mDim \033[0m"
+echo -e "Normal \033[4mUnderlined \033[0m"
+echo -e "Normal \033[5mBlink \033[0m"
+echo -e "Normal \033[7minverted \033[0m"
+echo -e "Normal \033[8mHidden \033[0m"
 
 
-echo -e "Normal \e[49mDefault ${NOCOLOR}"
-echo -e "Normal \e[40mBlack ${NOCOLOR}"
-echo -e "Normal \e[41mRed ${NOCOLOR}"
-echo -e "Normal \e[42mGreen ${NOCOLOR}"
-echo -e "Normal \e[43mYellow ${NOCOLOR}"
-echo -e "Normal \e[44mBlue ${NOCOLOR}"
-echo -e "Normal \e[45mMagenta ${NOCOLOR}"
-echo -e "Normal \e[46mCyan ${NOCOLOR}"
-echo -e "Normal \e[47mLight gray ${NOCOLOR}"
-echo -e "Normal \e[100mDark gray ${NOCOLOR}"
-echo -e "Normal \e[101mLight red ${NOCOLOR}"
-echo -e "Normal \e[102mLight green ${NOCOLOR}"
-echo -e "Normal \e[103mLight yellow ${NOCOLOR}"
-echo -e "Normal \e[104mLight blue ${NOCOLOR}"
-echo -e "Normal \e[105mLight magenta ${NOCOLOR}"
-echo -e "Normal \e[106mLight cyan ${NOCOLOR}"
-echo -e "Normal \e[107mWhite ${NOCOLOR}"
+echo -e "Normal \033[39mDefault \033[0m"
+echo -e "Normal \033[30mBlack \033[0m"
+echo -e "Normal \033[31mRed \033[0m"
+echo -e "Normal \033[32mGreen \033[0m"
+echo -e "Normal \033[33mYellow \033[0m"
+echo -e "Normal \033[34mBlue \033[0m"
+echo -e "Normal \033[35mMagenta \033[0m"
+echo -e "Normal \033[36mCyan \033[0m"
+echo -e "Normal \033[37mLight gray \033[0m"
+echo -e "Normal \033[90mDark gray \033[0m"
+echo -e "Normal \033[91mLight red \033[0m"
+echo -e "Normal \033[92mLight green \033[0m"
+echo -e "Normal \033[93mLight yellow \033[0m"
+echo -e "Normal \033[94mLight blue \033[0m"
+echo -e "Normal \033[95mLight magenta \033[0m"
+echo -e "Normal \033[96mLight cyan \033[0m"
+echo -e "Normal \033[97mWhite \033[0m"
+
+
+echo -e "Normal \033[49mDefault \033[0m"
+echo -e "Normal \033[40mBlack \033[0m"
+echo -e "Normal \033[41mRed \033[0m"
+echo -e "Normal \033[42mGreen \033[0m"
+echo -e "Normal \033[43mYellow \033[0m"
+echo -e "Normal \033[44mBlue \033[0m"
+echo -e "Normal \033[45mMagenta \033[0m"
+echo -e "Normal \033[46mCyan \033[0m"
+echo -e "Normal \033[47mLight gray \033[0m"
+echo -e "Normal \033[100mDark gray \033[0m"
+echo -e "Normal \033[101mLight red \033[0m"
+echo -e "Normal \033[102mLight green \033[0m"
+echo -e "Normal \033[103mLight yellow \033[0m"
+echo -e "Normal \033[104mLight blue \033[0m"
+echo -e "Normal \033[105mLight magenta \033[0m"
+echo -e "Normal \033[106mLight cyan \033[0m"
+echo -e "Normal \033[107mWhite \033[0m"
 
 echo " "
 
 for style in 0 1 2 3 4 5 6 7; do
     for fg in 39 30 31 32 33 34 35 36 37 90 91 92 93 94 95 96 97; do
         for bg in 49 40 41 42 43 44 45 46 47 100 101 102 103 104 105 106 107; do
-                echo -n -e "\e[${style};${fg};${bg}m${style};${fg};${bg}m ${NOCOLOR}"
+                echo -n -e "\033[${style};${fg};${bg}m${style};${fg};${bg}m \033[0m"
                 echo -n -e "\t"
         done
         echo
